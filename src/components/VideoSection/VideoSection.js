ab257f2ca8ee44d6b66eeaa1d197a0e567a41662
Bogdan Dyachenko <46127718+Bodyan-d@users.noreply.github.com>
2023-08-13 20:53:11 +0300

    added-main-video

--- src/components/VideoSection/VideoSection.js
+++ src/components/VideoSection/VideoSection.js
@@ -1,11 +1,37 @@
 import VideoPc from '../../video/video-pc-homepage.mp4';
+import { Link } from 'react-router-dom';
 
 function VideoSection(params) {
 	return (
 		<section className='main-section'>
-			<video autoplay muted preload className='main-video'>
-				<source src={VideoPc} type='video/mp4' />
-			</video>
+			<video
+				preload='auto'
+				autoplay=''
+				loop='loop'
+				playsinline=''
+				id='h-video'
+				src={VideoPc}
+				className='main-video'
+			></video>
+			<div className='main-desc'>
+				<h1>Trustless Cross-chain Bridges Made Practical</h1>
+				<p className='main-paragraph'>
+					The first trustless, efficient, and secure cross-chain
+					interoperability protocol with zero-knowledge proofs
+				</p>
+				<ul className='main-list'>
+					<li className='main-navigation-item navigation-item_rainbow'>
+						<Link to='/zknft' className='rainbow-link'>
+							zkBridge NFT
+						</Link>
+					</li>
+					<li className='main-navigation-item'>
+						<Link to='/zkmessenger' className='main-link'>
+							zkBridge Messenger
+						</Link>
+					</li>
+				</ul>
+			</div>
 		</section>
 	);
 }
